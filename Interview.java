99afce542b38c9c7fda4cc48fdf35fabcc57cee4
malinikalaiarasan <malinikalaiarasans1999@gmail.com>
2021-12-30 18:35:19 +0530

    committed

---
+++ Interview.java
@@ -0,0 +1,13 @@
+package org.tcs;
+
+public class Interview {
+	public static void main(String[] args) {
+	int	count=0;
+		
+		for(int i=1;i<=199;i++) {
+			if(i%2==0);
+			count++;
+			System.out.println("even num is"+count);
+		}
+}
+}
